d19707e3850b3ce6f7efa08d34d61c7b179307da
Luis Rovirosa Mairlot <luis@codium.team>
2020-11-16 17:13:41 +0100

    [coffee-machine] add the initial version

---
+++ coffee-machine/coffee_machine/coffee_machine.py
@@ -0,0 +1,3 @@
+class CoffeeMachine:
+
+   pass
---
+++ coffee-machine/tests/test_coffee_machine.py
@@ -0,0 +1,7 @@
+import unittest
+from coffee_machine import CoffeeMachine
+
+class CoffeeMachineTest(unittest.TestCase):
+
+    def test_xxx(self):
+        self.assertEqual(True, True)
