44e5220779bb5f58a868e66ed25c988ad0c07c09
Luis Rovirosa Mairlot <luis@codium.team>
2023-06-20 10:00:46 +0200

    expose duplicity

--- roman-numerals/roman_numerals/roman_numerals.py
+++ roman-numerals/roman_numerals/roman_numerals.py
@@ -2,15 +2,15 @@ class RomanNumerals:
 
     def convert(self, decimal: int) -> str:
         if decimal == 8:
-            return "VIII"
+            return "V" + self.convert(3)
         if decimal == 7:
-            return "VII"
+            return "V" + self.convert(2)
         if decimal == 6:
-            return "VI"
+            return "V" + self.convert(1)
         if decimal == 5:
-            return "V"
+            return "V" + self.convert(0)
         if decimal == 4:
             return "IV"
         if decimal >= 1:
-            return "I" + self.convert(decimal-1)
+            return "I" + self.convert(decimal - 1)
         return ""
