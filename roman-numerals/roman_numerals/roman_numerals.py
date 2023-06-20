132a79a390b4070add1023fe8232ee055b4e8bff
Luis Rovirosa Mairlot <luis@codium.team>
2023-06-20 09:56:10 +0200

    number 2

--- roman-numerals/roman_numerals/roman_numerals.py
+++ roman-numerals/roman_numerals/roman_numerals.py
@@ -1,4 +1,6 @@
 class RomanNumerals:
 
     def convert(self, decimal: int) -> str:
+        if decimal == 2:
+            return "II"
         return "I"
--- roman-numerals/tests/test_roman_numerals.py
+++ roman-numerals/tests/test_roman_numerals.py
@@ -8,7 +8,8 @@ from roman_numerals import RomanNumerals
 class RomanNumeralsTest(unittest.TestCase):
 
     @parameterized.expand([
-        (1, "I")
+        (1, "I"),
+        (2, "II"),
     ])
     def test_convert_decimal_numbers(self, decimal: int, expected_roman: str):
         roman_numerals = RomanNumerals()
