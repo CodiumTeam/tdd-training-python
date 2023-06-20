54cc362fca6e883bea72b5fba5cb66fe671843bb
Luis Rovirosa Mairlot <luis@codium.team>
2023-06-20 10:04:23 +0200

    second key in dict

--- roman-numerals/roman_numerals/roman_numerals.py
+++ roman-numerals/roman_numerals/roman_numerals.py
@@ -1,13 +1,14 @@
 class RomanNumerals:
 
     def convert(self, decimal: int) -> str:
-        conversion = {5: "V"}
+        conversion = {
+            5: "V",
+            4: "IV",
+        }
         for decimal_key in conversion.keys():
             decimal_value = conversion.get(decimal_key)
             if decimal >= decimal_key:
                 return decimal_value + self.convert(decimal - decimal_key)
-        if decimal >= 4:
-            return "IV" + self.convert(decimal - 4)
         if decimal >= 1:
             return "I" + self.convert(decimal - 1)
         return ""
