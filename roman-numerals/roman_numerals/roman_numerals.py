a3e4570c56a3546e19773740e458aa32eff3b14a
Luis Rovirosa Mairlot <luis@codium.team>
2023-06-20 10:04:34 +0200

    third key in dict

--- roman-numerals/roman_numerals/roman_numerals.py
+++ roman-numerals/roman_numerals/roman_numerals.py
@@ -4,11 +4,10 @@ class RomanNumerals:
         conversion = {
             5: "V",
             4: "IV",
+            1: "I",
         }
         for decimal_key in conversion.keys():
             decimal_value = conversion.get(decimal_key)
             if decimal >= decimal_key:
                 return decimal_value + self.convert(decimal - decimal_key)
-        if decimal >= 1:
-            return "I" + self.convert(decimal - 1)
         return ""
