60d3e8f58377dd321623fd67556f0a4fd805010d
Luis Rovirosa Mairlot <luis@codium.team>
2023-06-20 10:02:44 +0200

    small step to loop

--- roman-numerals/roman_numerals/roman_numerals.py
+++ roman-numerals/roman_numerals/roman_numerals.py
@@ -1,8 +1,10 @@
 class RomanNumerals:
 
     def convert(self, decimal: int) -> str:
-        if decimal >= 5:
-            return "V" + self.convert(decimal - 5)
+        decimal_key = 5
+        decimal_value = "V"
+        if decimal >= decimal_key:
+            return decimal_value + self.convert(decimal - decimal_key)
         if decimal >= 4:
             return "IV" + self.convert(decimal - 4)
         if decimal >= 1:
