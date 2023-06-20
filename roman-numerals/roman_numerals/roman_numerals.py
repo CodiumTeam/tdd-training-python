5b1d793e69e6b9d6f1fad65e2c9935b846e3f88e
Luis Rovirosa Mairlot <luis@codium.team>
2023-06-20 09:57:11 +0200

    increase symmetry

--- roman-numerals/roman_numerals/roman_numerals.py
+++ roman-numerals/roman_numerals/roman_numerals.py
@@ -1,8 +1,9 @@
 class RomanNumerals:
 
     def convert(self, decimal: int) -> str:
+        if decimal == 1:
+            return "I"
         if decimal == 2:
             return "II"
         if decimal == 3:
             return "III"
-        return "I"
