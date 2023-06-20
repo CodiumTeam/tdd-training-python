3053bbb34a65c412443c2445c83fb3fe293ea1b9
Luis Rovirosa Mairlot <luis@codium.team>
2023-06-20 10:01:03 +0200

    increase duplicity

--- roman-numerals/roman_numerals/roman_numerals.py
+++ roman-numerals/roman_numerals/roman_numerals.py
@@ -2,13 +2,13 @@ class RomanNumerals:
 
     def convert(self, decimal: int) -> str:
         if decimal == 8:
-            return "V" + self.convert(3)
+            return "V" + self.convert(decimal - 5)
         if decimal == 7:
-            return "V" + self.convert(2)
+            return "V" + self.convert(decimal - 5)
         if decimal == 6:
-            return "V" + self.convert(1)
+            return "V" + self.convert(decimal - 5)
         if decimal == 5:
-            return "V" + self.convert(0)
+            return "V" + self.convert(decimal - 5)
         if decimal == 4:
             return "IV"
         if decimal >= 1:
