439d8ab4047d777e7e1fedfc682249353ebaa7c1
Luis Rovirosa Mairlot <luis@codium.team>
2023-06-20 09:58:12 +0200

    increase duplicity

--- roman-numerals/roman_numerals/roman_numerals.py
+++ roman-numerals/roman_numerals/roman_numerals.py
@@ -2,8 +2,9 @@ class RomanNumerals:
 
     def convert(self, decimal: int) -> str:
         if decimal == 1:
-            return "I" + ""
+            return "I" + self.convert(0)
         if decimal == 2:
-            return "I" + "I"
+            return "I" + self.convert(1)
         if decimal == 3:
-            return "I" + "II"
+            return "I" + self.convert(2)
+        return ""
