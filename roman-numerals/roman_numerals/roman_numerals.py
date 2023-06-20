b23f3d43f2e32e7b53abbfd9d23c398842ae2865
Luis Rovirosa Mairlot <luis@codium.team>
2023-06-20 10:04:02 +0200

    first key in dict

--- roman-numerals/roman_numerals/roman_numerals.py
+++ roman-numerals/roman_numerals/roman_numerals.py
@@ -1,10 +1,11 @@
 class RomanNumerals:
 
     def convert(self, decimal: int) -> str:
-        decimal_key = 5
-        decimal_value = "V"
-        if decimal >= decimal_key:
-            return decimal_value + self.convert(decimal - decimal_key)
+        conversion = {5: "V"}
+        for decimal_key in conversion.keys():
+            decimal_value = conversion.get(decimal_key)
+            if decimal >= decimal_key:
+                return decimal_value + self.convert(decimal - decimal_key)
         if decimal >= 4:
             return "IV" + self.convert(decimal - 4)
         if decimal >= 1:
