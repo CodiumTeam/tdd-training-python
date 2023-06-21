89f1fe5333922ba45f79c7274502d51946092ad7
Luis Rovirosa Mairlot <luis@codium.team>
2023-06-21 12:26:15 +0200

    [user-registration] refactor: setup

--- user-registration/tests/test_user_registration.py
+++ user-registration/tests/test_user_registration.py
@@ -9,13 +9,15 @@ from user_registration.user_repository import UserRepository
 
 class RegisterUserTest(unittest.TestCase):
 
+    def setUp(self):
+        self.user_id_generator = Mock(UserIdGenerator)
+        self.user_repository = Mock(UserRepository)
+
     def test_the_user_is_persisted_with_the_id(self):
-        user_repository = Mock(UserRepository)
-        user_id_generator = Mock(UserIdGenerator)
-        user_id_generator.generate = Mock(return_value="anId")
-        register_user = RegisterUser(user_repository, user_id_generator)
+        self.user_id_generator.generate = Mock(return_value="anId")
+        register_user = RegisterUser(self.user_repository, self.user_id_generator)
 
         register_user.register("an@email", "valid_password")
 
         expected_user = User("anId", "an@email", "valid_password")
-        user_repository.save.assert_called_with(expected_user)
+        self.user_repository.save.assert_called_with(expected_user)
