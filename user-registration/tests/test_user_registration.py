d24698d906a6a9ae7ece1d4c2948d5475c8e7eb8
Luis Rovirosa Mairlot <luis@codium.team>
2023-06-21 12:48:16 +0200

    [user-registration] improve setup

--- user-registration/tests/test_user_registration.py
+++ user-registration/tests/test_user_registration.py
@@ -15,12 +15,12 @@ class RegisterUserTest(unittest.TestCase):
         self.user_id_generator = Mock(UserIdGenerator)
         self.user_repository = Mock(UserRepository)
         self.user_repository.find_by_email = Mock(return_value=None)
+        self.register_user = RegisterUser(self.user_repository, self.user_id_generator)
 
     def test_the_user_is_persisted_with_the_id(self):
         self.user_id_generator.generate = Mock(return_value="anId")
-        register_user = RegisterUser(self.user_repository, self.user_id_generator)
 
-        register_user.register("an@email", "valid_password")
+        self.register_user.register("an@email", "valid_password")
 
         expected_user = User("anId", "an@email", "valid_password")
         self.user_repository.save.assert_called_with(expected_user)
@@ -28,25 +28,22 @@ class RegisterUserTest(unittest.TestCase):
     def test_cannot_exists_two_user_with_the_same_email(self):
         existing_user = User("anId", "an@email", "a_password")
         self.user_repository.find_by_email = Mock(return_value=existing_user)
-        register_user = RegisterUser(self.user_repository, self.user_id_generator)
 
-        register = lambda: register_user.register("an@email", "valid_password")
+        register = lambda: self.register_user.register("an@email", "valid_password")
 
         self.assertRaises(UserAlreadyExistsException, register)
         self.user_repository.save.assert_not_called()
 
     def test_do_not_register_the_user_when_password_has_8_character_or_less(self):
-        register_user = RegisterUser(self.user_repository, self.user_id_generator)
 
-        register = lambda: register_user.register("an@email", "_2345678")
+        register = lambda: self.register_user.register("an@email", "_2345678")
 
         self.assertRaises(InvalidPasswordException, register)
         self.user_repository.save.assert_not_called()
 
     def test_do_not_register_the_user_when_password_has_not_an_underscore(self):
-        register_user = RegisterUser(self.user_repository, self.user_id_generator)
 
-        register = lambda: register_user.register("an@email", "123456789")
+        register = lambda: self.register_user.register("an@email", "123456789")
 
         self.assertRaises(InvalidPasswordException, register)
         self.user_repository.save.assert_not_called()
